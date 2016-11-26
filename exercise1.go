62ccb99ced615d703a6487109781fe7f0378577b
rgmcb <rgmcb.lion@gmail.com>
2016-11-26 12:48:19 +0000

    adicionados 2

---
+++ exercise1.go
@@ -0,0 +1,15 @@
+ package main
+
+    import "fmt"
+
+    func main() {
+
+fmt.Println("Welcome to Exercise 1")
+
+      for i:=12; i>=0 ; i-- {
+
+    	   if i!=4 && i!=2 && i!=10 {
+         fmt.Println("i = ", i)
+       }
+    }
+    }
